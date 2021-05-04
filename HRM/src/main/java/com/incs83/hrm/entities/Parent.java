b9b74de0976351a9a4aec39d97d8d241c1994794
Akki4A <akipandey25@gmail.com>
2021-05-04 16:26:52 +0530

    Parent Class Added

---
+++ HRM/src/main/java/com/incs83/hrm/entities/Parent.java
@@ -0,0 +1,43 @@
+package com.incs83.hrm.entities;
+
+import java.util.Date;
+
+public class Parent {
+
+    private String createdBy;
+    private String updatedBy;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
