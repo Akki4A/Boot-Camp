2d316e98c621117a53bf29395349c1cf58327129
Akki4A <akipandey25@gmail.com>
2021-05-06 17:50:14 +0530

    Address Controller Created

---
+++ HRM/src/main/java/com/incs83/hrm/controller/AddressController.java
@@ -0,0 +1,41 @@
+package com.incs83.hrm.controller;
+
+import com.incs83.hrm.business.AddressService;
+import com.incs83.hrm.common.AddressRequest;
+import com.incs83.hrm.entities.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/address")
+public class AddressController {
+    @Autowired
+    private AddressService addressService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void createAddress(@RequestBody AddressRequest addressRequest){
+        addressService.createAddress(addressRequest);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Address> getAllAddress(){
+        return addressService.getAllAddress();
+    }
+
+    @RequestMapping(path = "/{id}",method = RequestMethod.GET)
+    public Address getAddressById(@PathVariable Integer id){
+        return addressService.getAddressById(id);
+    }
+
+    @RequestMapping(path = "{id}",method = RequestMethod.PUT)
+    public void updateAddressByID(@RequestBody AddressRequest addressRequest, @PathVariable Integer id){
+        addressService.updateAddressById(addressRequest,id);
+    }
+
+    @RequestMapping(path = "/{id}",method = RequestMethod.DELETE)
+    public void deleteAddressById(@PathVariable Integer id){
+        addressService.deleteAddressById(id);
+    }
+}
